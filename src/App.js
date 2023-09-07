1f664c59f57775abf7d0836009875e96163fbae8
rajemurugesh <82995662+rajemurugesh@users.noreply.github.com>
2023-09-07 11:28:55 +0530

    build audio player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AudioPlayer from "./Audioplayer";
+
+const AUDIO_FILE = "https://storiestogrowby.org/wp-content/uploads/2016/03/Fur-Feather-Africa.mp3?_=1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <AudioPlayer audioSrc={AUDIO_FILE} />
     </div>
   );
 }
