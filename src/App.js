39e2a53f6ab97a369dc099ed63efa602ca5f450b
rajemurugesh <82995662+rajemurugesh@users.noreply.github.com>
2023-09-07 18:54:29 +0530

    playing video

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-import AudioPlayer from "./Audioplayer";
+import ReactPlayer from "react-player";
+import "./App.css";
+// const VIDEO_FILE =
+//   "https://www.youtube.com/watch?v=fY-Wq0k_9P4&list=PLC3y8-rFHvwheJHvseC3I0HuYI2f46oAK&index=22";
 
-const AUDIO_FILE = "https://storiestogrowby.org/wp-content/uploads/2016/03/Fur-Feather-Africa.mp3?_=1";
-
-function App() {
+const VIDEO_FILE = "./playvideo.mp4"
+const App = () => {
   return (
-    <div className="container">
-      <AudioPlayer audioSrc={AUDIO_FILE} />
+    <div className="App">
+      <ReactPlayer 
+      
+      width="480px" 
+      height="240px" 
+      controls url={VIDEO_FILE} 
+      onReady={() => console.log('onReady Callback')}
+      onStart={() => console.log('onStart Callback')}
+      onPause={() => console.log('onPause Callback')}
+      onEnded={() => console.log('onEnded Callback')}
+      onError={() => console.log('onError Callback')}
+      />
     </div>
   );
-}
-
+};
 export default App;
